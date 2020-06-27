e851fd89aa33ed141f783691e3cf80a56a938864
Krime Loma <hulitiho@huli.ru>
2020-06-27 22:15:47 +0800

    Initial Commit

---
+++ Chinee Dictionary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chinee Dictionary
+//
+//  Created by Krime Loma on 6/27/20.
+//  Copyright © 2020 Rakk Kiryl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
